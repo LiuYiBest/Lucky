0ea176b27e11999f17f6c77a9cbebbb62f66c74c
LiuyiBest <1070692044@qq.com>
2021-08-03 22:55:20 +0800

    封装SVG组件，创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="nav">
-    <router-link to="money">记账</router-link>
+    <router-link to="/money">记账
+      <Icon name="money"/>
+    </router-link>
     |
-    <router-link to="labels">标签</router-link>
+    <router-link to="/labels">明细
+      <Icon name="labels"/>
+    </router-link>
     |
-    <router-link to="statistics">统计</router-link>
+    <router-link to="/statistics">统计
+      <Icon name="statistics"/>
+    </router-link>
   </div>
 </template>
 
+
 <script lang="ts">
+
 export default {
   name: 'Nav'
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
-    display: flex;
-  }
+.nav {
+  border: 1px solid red;
+  display: flex;
+}
 </style>
