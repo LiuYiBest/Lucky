e9804fbbe0c79d253f8e2b73bce17ab30821a11c
LiuyiBest <1070692044@qq.com>
2021-08-03 16:49:29 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <h1>404</h1>
+      <h1>你访问的路径不存在，请重新检查网站是否正确</h1>
+    </div>
+    <div>
+      <router-link to="/">返回招财猫记账首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
