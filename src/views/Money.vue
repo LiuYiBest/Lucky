1ffab5ff70d73e8495bc358f4289028895fd0f49
LiuyiBest <1070692044@qq.com>
2021-08-04 23:30:42 +0800

    Labels样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,43 +1,37 @@
 <template>
   <Layout class-prefix="layout">
-
-    <NumberPad :value.sync="record.amount" @submit="saveRecord"></NumberPad>
-    <Types :value.sync="record.type"></Types>
-    <Notes @update:value="onUpdateNotes"></Notes>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"></Tags>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
 <script lang="ts">
-
+import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-
-import Vue from 'vue';
 import {Component, Watch} from 'vue-property-decorator';
 import model from '@/model';
-
 const recordList = model.fetch();
 
-// window.localStorage.setItem('version', '1.0.1');
 
 @Component({
-  components: {Tags, Notes, Types, NumberPad},
+  components: {Tags, Notes, Types, NumberPad}
 })
 
+
 export default class Money extends Vue {
-  tags = ['其它', '衣', '食', '住', '行'];
-  recordList: RecordItem = recordList;
+  tags = ['其它','衣', '食', '住', '行'];
+  recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
-
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
-
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
@@ -50,8 +44,7 @@ export default class Money extends Vue {
   onRecordListChange() {
     model.save(this.recordList);
   }
-
-};
+}
 </script>
 
 <style lang="scss">
@@ -60,11 +53,5 @@ export default class Money extends Vue {
   flex-direction: column-reverse;
 }
 </style>
-<style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
 
 
-</style>
-
-
-<!--</style>-->
