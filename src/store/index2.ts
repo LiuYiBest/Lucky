37f77dec67192714db1182deed104ce22221241a
LiuyiBest <1070692044@qq.com>
2021-08-05 19:26:43 +0800

    解决window依赖 以及全局变量过多

---
+++ src/store/index2.ts
@@ -0,0 +1,34 @@
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
+
+const store = {
+    //record仓库
+    recordList: recordListModel.fetch(),
+    createRecord: (record: RecordItem) => {
+        recordListModel.creat(record);
+    },
+    //1.全局变量太多
+    //2.严重依赖window
+
+    //tag仓库
+    tagList: tagListModel.fetch(),
+    findTag(id: string) {
+        return this.tagList.filter(t => t.id === id)[0];
+    },
+    createTag: (name: string) => {
+        const message = tagListModel.create(name);
+        if (message === 'duplicated') {
+            window.alert('标签名已存在，请重新输入');
+        } else if (message === 'success') {
+            window.alert('成功保存');
+        }
+    },
+    removeTag: (id: string) => {
+        return tagListModel.remove(id);
+    },
+    updateTag: (id: string, name: string) => {
+        return tagListModel.update(id, name);
+    },
+};
+
+export default store;
