74797fc20b16dce3602fe4335b2b463bfa15259d
LiuyiBest <1070692044@qq.com>
2021-08-05 11:38:31 +0800

    新增LabelEdit

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,16 +1,29 @@
 <template>
   <Layout>
-    123edit
+    编辑
   </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import tagListModel from '@/models/tagListModel';
 
 @Component
 export default class EditLabel extends Vue {
-
+  created() {
+    // console.log(this.$route.params)
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    // //fillte一定会返回数组
+    const tag = tags.filter(t => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace('/404');
+    }
+  }
 }
 </script>
 
