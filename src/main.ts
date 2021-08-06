68200da6414c3fccead9cd7f10b560bc3d07be1d
LiuyiBest <1070692044@qq.com>
2021-08-06 22:33:33 +0800

    提示手机预览

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,20 @@ new Vue({
     store,
     render: h => h(App)
 }).$mount('#app');
+
+
+if (document.documentElement.clientWidth) {
+    window.alert('为了预览效果，请使用手机打开招财猫记账');
+    const img = document.createElement('img');
+    img.src = '/ma.png';
+    img.style.position = 'fixed';
+    img.style.left = '50%';
+    img.style.top = '50%';
+    img.style.transform = 'translate(-50%,-50%)';
+    img.style.boxShadow = ' 0 0 10px rgba(0,0,0,0.3)';
+    document.body.appendChild(img);
+    img.onclick = () => {
+        img.style.visibility = 'hidden';
+    };
+
+}
