9e847751c2e28c13633386f4c3a5dc784a158909
LiuyiBest <1070692044@qq.com>
2021-08-06 11:02:48 +0800

    EditLabel使用Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -5,11 +5,10 @@
       <span class="title">编辑标签</span>
       <span class="rightIcon"/>
     </div>
-    <!--    @update:value = "updateTag($)"-->
     <div class="form-wrapper">
       <FormItem :value="tag.name"
                 @update:value="update"
-                field-name="标签名：" placeholder="请输入标签名"/>
+                field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button @click="remove">删除标签</Button>
@@ -22,46 +21,39 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
-
 @Component({
-
+  components: {Button, FormItem},
 })
 export default class EditLabel extends Vue {
-  get tag(){
-    return this.$store.state.constructor
+  get tag() {
+    return this.$store.state.currentTag;
   }
-
   created() {
     const id = this.$route.params.id;
-    this.$store.commit('setCurrentTag',id);
-   if (!this.tag){
-     this.$router.replace('/404')
-   }
+    this.$store.commit('fetchTags');
+    this.$store.commit('setCurrentTag', id);
+    if (!this.tag) {
+      console.log('no tag');
+      this.$router.replace('/404');
+    } else {
+      console.log('has tag');
+    }
   }
-
   update(name: string) {
     if (this.tag) {
-      //TODO
-      // store.updateTag(this.tag.id, name);
+      this.$store.commit('updateTag', {
+        id: this.tag.id, name
+      });
     }
   }
-
   remove() {
     if (this.tag) {
-      //TODO
-      return
-      // if (store.removeTag(this.tag.id)) {
-      //   this.$router.back();Tags
-      // } else {
-      //   window.alert('删除失败');
-      // }
+      this.$store.commit('removeTag', this.tag.id);
     }
   }
-
   goBack() {
     this.$router.back();
   }
-
 }
 </script>
 
@@ -74,26 +66,21 @@ export default class EditLabel extends Vue {
   display: flex;
   align-items: center;
   justify-content: space-between;
-
   > .title {
   }
-
   > .leftIcon {
     width: 24px;
     height: 24px;
   }
-
   > .rightIcon {
     width: 24px;
     height: 24px;
   }
 }
-
 .form-wrapper {
   background: white;
   margin-top: 8px;
 }
-
 .button-wrapper {
   text-align: center;
   padding: 16px;
