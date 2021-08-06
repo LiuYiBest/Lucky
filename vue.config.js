9bcb41c06023a06652f7422b0e31bf8b507ab7b7
LiuyiBest <1070692044@qq.com>
2021-08-06 20:28:00 +0800

    GitHubPages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@
 const path = require('path')
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/Lucky-1/'
+        : '/',
+
     lintOnSave: false,
     chainWebpack: config => {
         const dir = path.resolve(__dirname, 'src/assets/icons')
@@ -11,9 +15,9 @@ module.exports = {
             .test(/\.svg$/)
             .include.add(dir).end() // 包含 icons 目录
             .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-            // .use('svgo-loader').loader('svgo-loader')
-            // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
-            // .end()
+        // .use('svgo-loader').loader('svgo-loader')
+        // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+        // .end()
         // .use('svgo-loader').loader('svgo-loader')
         // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
         // eslint-disable-next-line @typescript-eslint/no-var-requires
