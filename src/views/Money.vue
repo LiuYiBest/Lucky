1f95451c02a85907c4c073b62996808f5deea10d
LiuyiBest <1070692044@qq.com>
2021-08-17 12:52:07 +0800

    修改

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,7 +16,7 @@
                 :value.sync="record.notes"
       />
     </div>
-    <Tags/>
+    <Tags @update:value="record.tags = $event"/>
   </Layout>
 </template>
 
@@ -37,7 +37,7 @@ export default class Money extends Vue {
   }
   recordTypeList = recordTypeList;
   record: RecordItem = {
-    tags: [], notes: '', type: '-', amount: 0,createdAt:new Date().toISOString()
+    tags: [], notes: '', type: '-', amount: 0, createdAt: new Date().toISOString()
   };
   created() {
     this.$store.commit('fetchRecords');
@@ -46,7 +46,14 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
+    if (!this.record.tags || this.record.tags.length === 0) {
+      return window.alert('请至少选择一个标签');
+    }
     this.$store.commit('createRecord', this.record);
+    if (this.$store.state.createRecordError === null) {
+      window.alert('已保存');
+      this.record.notes = '';
+    }
   }
 }
 </script>
