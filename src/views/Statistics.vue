a8aca2aa41fc3ab9f68596c676f5096419d429e8
LiuyiBest <1070692044@qq.com>
2021-08-17 18:32:44 +0800

    options更新

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,18 +2,18 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <div class="chart-wrapper" ref="chartWrapper">
-      <Chart class="chart" :options="x"/>
+      <Chart class="chart" :options="chartOptions"/>
     </div>
     <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
-        <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
+        <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
         <ol>
           <li v-for="item in group.items" :key="item.id"
               class="record"
           >
-            <span>{{tagString(item.tags)}}</span>
-            <span class="notes">{{item.notes}}</span>
-            <span>￥{{item.amount}} </span>
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }} </span>
           </li>
         </ol>
       </li>
@@ -33,6 +33,7 @@ import clone from '@/lib/clone';
 import Chart from '@/components/Chart.vue';
 import _ from 'lodash';
 import day from 'dayjs';
+
 @Component({
   components: {Tabs, Chart},
 })
@@ -41,10 +42,13 @@ export default class Statistics extends Vue {
     return tags.length === 0 ? '无' :
         tags.map(t => t.name).join('，');
   }
+
   mounted() {
+    console.log(this.groupedList);
     const div = (this.$refs.chartWrapper as HTMLDivElement);
     div.scrollLeft = div.scrollWidth;
   }
+
 
   beautify(string: string) {
     const day = dayjs(string);
@@ -63,35 +67,36 @@ export default class Statistics extends Vue {
     }
   }
 
-  get y() {
+  get keyValueList() {
     const today = new Date();
     const array = [];
     for (let i = 0; i <= 29; i++) {
-      // this.recordList = [{date:7.3, value:100}, {date:7.2, value:200}]
       const dateString = day(today)
           .subtract(i, 'day').format('YYYY-MM-DD');
-      const found = _.find(this.recordList, {
-        createdAt: dateString
+      const found = _.find(this.groupedList, {
+        title: dateString
       });
       array.push({
-        date: dateString, value: found ? found.amount : 0
+        key: dateString, value: found ? found.total : 0
       });
     }
     array.sort((a, b) => {
-      if (a.date > b.date) {
+      if (a.key > b.key) {
         return 1;
-      } else if (a.date === b.date) {
+      } else if (a.key === b.key) {
         return 0;
       } else {
         return -1;
       }
     });
     return array;
+
   }
 
-  get x() {
-    const keys = this.y.map(item => item.date);
-    const values = this.y.map(item => item.value);
+  get chartOptions() {
+    const keys = this.keyValueList.map(item => item.key);
+    const values = this.keyValueList.map(item => item.value);
+
     return {
       grid: {
         left: 0,
@@ -104,7 +109,7 @@ export default class Statistics extends Vue {
         axisLine: {lineStyle: {color: '#666'}},
         axisLabel: {
           formatter: function (value: string, index: number) {
-            return value.substr(5)
+            return value.substr(5);
           }
         }
       },
@@ -137,7 +142,9 @@ export default class Statistics extends Vue {
     const newList = clone(recordList)
         .filter(r => r.type === this.type)
         .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
-    if (newList.length === 0) {return [];}
+    if (newList.length === 0) {
+      return [];
+    }
     type Result = { title: string, total?: number, items: RecordItem[] }[]
     const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
     for (let i = 1; i < newList.length; i++) {
@@ -156,9 +163,11 @@ export default class Statistics extends Vue {
     });
     return result;
   }
+
   beforeCreate() {
     this.$store.commit('fetchRecords');
   }
+
   type = '-';
   recordTypeList = recordTypeList;
 }
@@ -169,24 +178,30 @@ export default class Statistics extends Vue {
   max-width: 100%;
   height: 400px;
 }
+
 .noResult {
   padding: 16px;
   text-align: center;
 }
+
 ::v-deep {
   .type-tabs-item {
     background: #C4C4C4;
+
     &.selected {
       background: white;
+
       &::after {
         display: none;
       }
     }
   }
+
   .interval-tabs-item {
     height: 48px;
   }
 }
+
 %item {
   padding: 8px 16px;
   line-height: 24px;
@@ -194,22 +209,28 @@ export default class Statistics extends Vue {
   justify-content: space-between;
   align-content: center;
 }
+
 .title {
   @extend %item;
 }
+
 .record {
   background: white;
   @extend %item;
 }
+
 .notes {
   margin-right: auto;
   margin-left: 16px;
   color: #999;
 }
+
 .chart {
   width: 430%;
+
   &-wrapper {
     overflow: auto;
+
     &::-webkit-scrollbar {
       display: none;
     }
