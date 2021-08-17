f833ab898b2d749549552099895dcbf1b0e10f7b
LiuyiBest <1070692044@qq.com>
2021-08-17 14:20:56 +0800

    完成chart组件封装

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+    <Chart :options="x"/>
     <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
@@ -16,7 +17,7 @@
       </li>
     </ol>
     <div v-else class="noResult">
-      还未开始记账，招财猫正在等待
+      目前没有相关记录
     </div>
   </Layout>
 </template>
@@ -27,8 +28,9 @@ import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
 @Component({
-  components: {Tabs},
+  components: {Tabs, Chart},
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
@@ -41,6 +43,7 @@ export default class Statistics extends Vue {
     if (day.isSame(now, 'day')) {
       return '今天';
     } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      console.log('hi');
       return '昨天';
     } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
       return '前天';
@@ -49,6 +52,31 @@ export default class Statistics extends Vue {
     } else {
       return day.format('YYYY年M月D日');
     }
+  }
+  get x() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: [
+          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [
+          820,   932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+        ],
+        type: 'line'
+      }],
+      tooltip: {show: true}
+    };
   }
   get recordList() {
     return (this.$store.state as RootState).recordList;
@@ -72,6 +100,8 @@ export default class Statistics extends Vue {
     }
     result.map(group => {
       group.total = group.items.reduce((sum, item) => {
+        console.log(sum);
+        console.log(item);
         return sum + item.amount;
       }, 0);
     });
@@ -86,6 +116,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
+.echarts {
+  max-width: 100%;
+  height: 400px;
+}
 .noResult {
   padding: 16px;
   text-align: center;
